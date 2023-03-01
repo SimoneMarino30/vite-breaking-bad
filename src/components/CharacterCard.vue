f00e7915c9ffbaba72bd1fb36e5b44cf522db7c8
Simone Marino <simo.marino@outlook.it>
2023-03-01 19:13:45 +0100

    Components CarcList and Card Done

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,7 +1,30 @@
 <script>
-export default {};
+export default {
+  props: {
+    pic: String,
+    name: String,
+    archetype: String,
+    attribute: String,
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div class="card-box col text-center">
+    <img :src="pic" alt="" class="img-fluid" />
+    <h5>{{ name }}</h5>
+    <p>{{ archetype }}</p>
+    <p>{{ attribute }}</p>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h5 {
+  color: green;
+  text-transform: uppercase;
+}
+
+img {
+  min-height: 2rem;
+}
+</style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,15 +1,48 @@
 <script>
-// import "../components/CharacterCard.vue";
-import
+import axios from "axios";
+import CharacterCard from "./CharacterCard.vue";
 
-export default {};
+export default {
+  data() {
+    return {
+      cards_list: [],
+    };
+  },
+
+  components: { CharacterCard },
+
+  created() {
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0")
+      .then((resp) => {
+        console.log(resp);
+        this.cards_list = resp.data.data;
+      });
+  },
+};
 </script>
 
 <template>
   <div class="big-card-border m-2">Found TOT cards</div>
   <div class="container">
-    <div class="card-box"></div>
+    <div class="row row-cols-5">
+      <CharacterCard
+        v-for="card in cards_list"
+        :pic="card.card_images[0].image_url"
+        :name="card.name"
+        :archetype="card.archetype"
+        :attribute="card.attribute"
+      />
+      <!-- <img :src="" alt="" class="img-fluid" />
+        <h5>{{  }}</h5>
+        <p>{{  }}</p>
+        <p>{{  }}</p> -->
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.row {
+  border: 3px dashed blue;
+}
+</style>
